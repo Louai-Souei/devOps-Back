591d71d61fcff753b387ffbffa4ec684c38d8ffb
Louai-Souei <louai.souei04@gmail.com>
2024-12-31 16:11:47 +0100

    First Commit

---
+++ src/test/java/simple/crud/devOps/DevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package simple.crud.devOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
